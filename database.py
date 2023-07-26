8d0cafa2f6e1115dfbcc23811f2844be7ee4d45b
Leon Xu <leonxu0204@gmail.com>
2023-07-26 01:37:33 -0400

    added toptracks and update command

--- database.py
+++ database.py
@@ -1,13 +1,39 @@
 import pymongo
 import lastfmbot
+import datetime
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mongoDB = client["lastfmbot"]
 users = mongoDB["users"]
 
+def set_default_user(user, default_user):
+    print(user, default_user)
+    current = users.find_one({"username": user})
+    if (current == None):
+        insert = { "username": user, "default_user": default_user } 
+        users.insert_one(insert)
+    else:
+        users.update_one({"username": user}, { "$set": { "default_user": default_user } })
+    
 
-def test():
-    mydict = { "name": "Test", "address": "Test Highway" }
-    x = users.insert_one(mydict)
-    print(x.inserted_id)
+def get_default_user(user):
+    current = users.find_one({"username": user})
+    return current["default_user"]
+
+def insert_toptracks(user, tracks, time):
+    current_time = datetime.datetime.now()
+    users.update_one({"username": user}, { "$set": { ("toptrack" + time): {"tracks": tracks, "lastupdated": current_time} } })
+
+def get_toptracks(user, time):
+    current = users.find_one({"username": user, ("toptrack" + time) : {"$exists" : True}})
+    
+    if (current != None):
+        return current[("toptrack" + time)]
+    else:
+        return None
+
+def update(user):
+    default_user = users.find_one({"username": user})["default_user"]
+    users.delete_one({"username": user})
+    users.insert_one({"username": user, "default_user": default_user})
--- lastfmbot.py
+++ lastfmbot.py
@@ -44,8 +44,14 @@ def get_userinfo(user):
     result = requests.post(ROOT_URL + url)
     return(result.json())
 
-def get_usertoptracks(user):
-    url = "?method=user.gettoptracks&user=" + user + "&api_key=" + API_KEY + "&format=json&limit=5"
+def get_usertoptracks(user, time=None):
+    if (time != None and time != "overall" and time != "7day" and time != "1month" and time != "3month" and time != "6month" and time != "12month"):
+        pass
+    url = ''
+    if (time == None):
+        url = "?method=user.gettoptracks&user=" + user + "&api_key=" + API_KEY + "&format=json&limit=10"
+    else:
+        url = "?method=user.gettoptracks&user=" + user + "&api_key=" + API_KEY + "&format=json&limit=10&period=" + time
     result = requests.post(ROOT_URL + url)
     return(result.json()["toptracks"]["track"])
 
@@ -84,8 +90,7 @@ async def login(ctx):
 
 @bot.command()
 async def set(ctx, user):
-    accounts[ctx.author] = user
-    database.test()
+    database.set_default_user(ctx.author.name, user)
     await ctx.send("You successfully set your username!")
 
 @bot.command()
@@ -100,20 +105,51 @@ async def info(ctx, user=None):
         await ctx.send(get_userinfo(accounts[ctx.author]))
 
 @bot.command()    
-async def topSongs(ctx, user):
-    result = get_usertoptracks(user)
-    message = [track['name'] for track in result]
-    await ctx.send("```" + '\n' + '\n'.join(message) + "```")
+async def toptracks(ctx, time="overall"):
+    user = database.get_default_user(ctx.author.name)
+    result = database.get_toptracks(ctx.author.name, time)
+    if (result == None):
+        database.insert_toptracks(ctx.author.name, get_usertoptracks(user, time), time="overall")
+        result = database.get_toptracks(ctx.author.name, time)
+    print(result)
+    message = [(track['name'] + " - " + track["playcount"] + " times") for track in result["tracks"]]
+    current_time = result["lastupdated"]
+    for i in range(len(message)):
+        message[i] = str(i + 1) + ". " + message[i]
+    
+    if (time == "overall"):
+        await ctx.send("```" + '\nOverall, you listened to\n' + '\n'.join(message) + "\nlast updated: " + current_time.strftime("%Y-%m-%d %H:%M:%S") + "```")
+    elif (time == "7day"):
+        await ctx.send("```" + '\n' + "For the past week, you listened to " + '\n' + '\n'.join(message) + "```")
+    elif (time == "1month"):
+        await ctx.send("```" + '\n' + "For the past month, you listened to " + '\n' + '\n'.join(message) + "```")
+    elif (time == "3month"):
+        await ctx.send("```" + '\n' + "For the past 3 months, you listened to " + '\n' + '\n'.join(message) + "```")
+    elif (time == "6month"):
+        await ctx.send("```" + '\n' + "For the past 6 months, you listened to " + '\n' + '\n'.join(message) + "```")
+    elif (time == "12month"):
+        await ctx.send("```" + '\n' + "For the past year, You listened to " + '\n' + '\n'.join(message) + "```")
 
 @bot.command()    
 async def topArtists(ctx, user):
     result = get_usertopartists(user)
     message = [artist['name'] for artist in result]
+    for i in range(len(message)):
+        message[i] = (i + 1) + ". " + message[i]
     print(message)
     await ctx.send("```" + '\n' + '\n'.join(message) + "```")
 
 @bot.command()
+async def update(ctx):
+    database.update(ctx.author.name)
+    await ctx.send("Your data has been updated!")
+
+@bot.command()
 async def h(ctx):
-    help = "```$login: authorize access \n$info [username]: get user info \n$top [user]: show top 5 tracks for the user```"
+    help = """```$login: authorize access\n
+$set [username]: set your last.fm uesrname\n
+$toptrack (overall | 7day | 1month | 3month | 6month | 12month): show top 10 tracks for the user in a given timeframe\n
+$update: update all to the latest data```"""
     await ctx.send(help)
+
 bot.run(TOKEN)
